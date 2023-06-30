afae22a87902763a23b410c59bec5cb2c71856d9
Ehsan <eaca89@gmail.com>
2023-06-30 14:41:11 -0400

    password reset done

--- config/settings.py
+++ config/settings.py
@@ -138,3 +138,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
