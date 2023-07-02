f1ac069f8f450f794d7b94a7306299b4d03d1674
Ehsan <eaca89@gmail.com>
2023-07-01 22:12:04 -0400

    comments part is done

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from datetime import date
 from django.urls import reverse
+from django.utils import timezone
 
 
 class Post(models.Model):
@@ -16,3 +17,13 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('post_detail', kwargs={'pk': self.pk})
+
+
+class Comment(models.Model):
+    author = models.ForeignKey('accounts.CustomUser', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    body = models.TextField(null=False, blank=False)
+    date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.body
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,12 @@
-from django.views.generic import View, CreateView, DetailView
+from typing import Any, Dict
+from django.views.generic import View, CreateView, DetailView, FormView
 from django.views.generic.edit import UpdateView, DeleteView
 from .models import Post
-from .forms import PostForm
-from django.urls import reverse_lazy
+from .forms import PostForm, CommentForm
+from django.urls import reverse_lazy, reverse
 from django.core.paginator import Paginator
 from django.shortcuts import render
+from django.views.generic.detail import SingleObjectMixin
 
 
 class HomeView(View):
@@ -26,9 +28,46 @@ class HomeView(View):
         return render(request, self.template_name, context)
 
 
-class PostDetailView(DetailView):
+class CommentGet(DetailView):
     model = Post
     template_name = 'post_single.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = CommentForm()
+        return context
+
+
+class CommentPost(SingleObjectMixin, FormView):
+    model = Post
+    form_class = CommentForm
+    template_name = "post_single.html"
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super().post(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        comment = form.save(commit=False)
+        comment.post = self.object
+        comment.author = self.request.user
+        comment.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        post = self.get_object()
+        return reverse("post_detail", kwargs={'pk': post.pk})
+
+
+class PostDetailView(View):
+
+    def get(self, request, *args, **kwargs):
+        view = CommentGet.as_view()
+        return view(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        view = CommentPost.as_view()
+        return view(request, *args, **kwargs)
 
 
 class PostNewView(CreateView):
